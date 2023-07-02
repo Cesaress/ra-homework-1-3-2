10d2197dbd9594581447c013f5682f815da5a9d5
Андрей Богданов <cesaress@yandex.ru>
2023-07-02 15:51:37 +0300

    hw 1-3-2

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import Listing from "./components/Listing.js";
+import Etsy from "./etsy.json";
+
+function App() {
+  return <Listing items={Etsy} />;
+}
+
+export default App;
